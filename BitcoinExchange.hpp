b0d5a792a04a40c2bcd3d4b3f629e256340ec7c0
Maxime Desroses <mdesrose@1E5.42angouleme.fr>
2024-02-09 18:37:52 +0100

    m

---
+++ BitcoinExchange.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mdesrose <mdesrose@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/25 14:15:38 by mdesrose          #+#    #+#             */
+/*   Updated: 2024/02/05 14:44:33 by mdesrose         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef _BITCOIN_
+#define _BITCOIN_
+
+class BitcoinExchange
+{
+private:
+    /* data */
+public:
+    BitcoinExchange();
+    ~BitcoinExchange();
+};
+
+BitcoinExchange::BitcoinExchange(/* args */)
+{
+}
+
+BitcoinExchange::~BitcoinExchange()
+{
+}
+
+#endif
