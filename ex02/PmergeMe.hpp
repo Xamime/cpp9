ba212ecf13e505573be5ba0248996bd3b8a2b4dc
maxime <maximedesroses96@gmail.com>
2024-02-13 15:53:08 +0100

    m

--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -6,7 +6,65 @@
 /*   By: maxime <maxime@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 15:13:33 by maxime            #+#    #+#             */
-/*   Updated: 2024/02/12 15:13:34 by maxime           ###   ########.fr       */
+/*   Updated: 2024/02/13 15:43:28 by maxime           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <vector>
+#include <cstdlib>
+#include <iostream>
+
+typedef struct s_data
+{
+    int a;
+    int b;
+}              data;
+
+class PmergeMe
+{
+    public:
+        PmergeMe();
+        PmergeMe(const PmergeMe& other);
+        PmergeMe& operator=(const PmergeMe& other);
+
+        ~PmergeMe();
+
+        void setProperty(Type value);
+        Type getProperty() const;
+
+    private:
+        std::vector<data> victor;
+};
+
+PmergeMe::PmergeMe()
+{
+    // TODO: Implement constructor
+}
+
+PmergeMe::PmergeMe(const PmergeMe& other)
+{
+    // TODO: Implement copy constructor
+}
+
+PmergeMe& PmergeMe::operator=(const PmergeMe& other)
+{
+    if (this != &other)
+    {
+        // TODO: Implement copy assignment
+    }
+    return *this;
+}
+
+PmergeMe::~PmergeMe() {
+    // TODO: Implement destructor
+}
+
+void PmergeMe::setProperty(Type value)
+{
+    // TODO: Implement setter
+}
+
+Type PmergeMe::getProperty() const
+{
+    // TODO: Implement getter
+}
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,11 +6,31 @@
 /*   By: maxime <maxime@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 15:12:54 by maxime            #+#    #+#             */
-/*   Updated: 2024/02/12 15:13:59 by maxime           ###   ########.fr       */
+/*   Updated: 2024/02/13 15:50:46 by maxime           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "PmergeMe.hpp"
+
+
 int main(int argc, char *argv[])
 {
-    
+    if (argc != 2)
+        return 1;
+    std::vector<data> test;
+    std::string args = argv[1];
+    size_t pos = args.find(' ');
+    while (pos != std::string::npos)
+    {
+        int first = atoi(args.c_str());
+        args.erase(0, pos +1);
+        pos = args.find(' ');
+        
+        int second = atoi(args.c_str());
+        args.erase(0, pos +1);
+        pos = args.find(' ');
+        
+        test.push_back((data){first, second});
+    }
+    std::cout << test[0].a;
 }
